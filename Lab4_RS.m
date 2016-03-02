e7884113bdb7db7846b3992940eb8951cdba07f8
GeoStoner <ryan.stoner@colorado.edu>
2016-03-02 10:45:21 -0700

    Modified and functional version of original code

--- Lab4_RS.m
+++ Lab4_RS.m
@@ -1,65 +1,98 @@
 % Model of glacier development and stabilization. Ryan Stoner Feb 21, 2016
 % For Geology modeling seminar
-clear all
+clear
+
+clc
 %% Initialize
+
+figure(1)
+clf
 
 
 
 zmax = 2550;            % m, maximum altitude
-ELA = 2500;             % m
-s = 0.2;               % slope
-dx = 1;               % m
-xmax = 200;           % m
+ELA = 2200;             % m
+s = 0.05;               % slope
+dx = 100;               % m
+xmax = 20000;           % m
 x = 0:dx:xmax;          % m
-zbas = zmax - s*x;          % m
-z = zbas;         % m
+zbas = zmax-s*x;        % m
+z = zbas;               % m
 
 gamma = 0.01;           % m/yr
 
-dt = 1;                 % yr
-tmax = 2000;              % yr
-t = 0:dt:tmax;            % yr
+dt = 0.002;             % yr
+tmax = 150;             % yr
+t = 0:dt:tmax;          % yr
+nplots = 20;
+tplot = tmax/nplots;
 
 N = 3;
 
 icedens = 917;          % kg/m^3
 g = 9.8;                % m/s^2
 W = 100;
-h = zeros(1,length(x));   % m, initial thickness of ice
-usl = 0.1;              % m/yr, sliding velocity of ice
+h = zeros(1,length(x)); % m, initial thickness of ice
+usl = 0.01;             % m/yr, sliding velocity of ice
 A = 2.1*10^-16;         % yr^-1,Pa^-3
+
+nframe = 0;
 %% Loop
 imax = length(t);
 for i=1:imax
 
-b = gamma*((zmax-zbas)-ELA)
+b = gamma*(z-ELA);
 dzdx = abs(diff(z)/dx);
 
 dHdx = diff(h)/dx;
-
-
 hedge = h(1:length(x)-1)+0.5*dHdx;
 
-Q = usl*hedge+ A*(icedens*g*abs(dzdx)).^3.*(hedge.^5)/5;
+Q = (usl*hedge)+ A*(icedens*g*abs(dzdx)).^3.*(hedge.^5)/5;
 
 Q = [0 Q 0];
 dHdt = b - 1/W*(diff(Q)/dt);
 
 
 
-% for i =1:imax
 
-% B = f(z) % not a function of bedrock
-
-% H
-% z = 
 
 h = h+dHdt*dt;
-z = zbas +h;
-%hold on
-%plot(x,z)
+h = max(h,0);
+z = zbas + h;
+
+if(mod(t(i),tplot)==0)
+nframe = nframe+1;
+    figure(1)
+   
+plot(x/1000,zbas,'k')
+hold on
+plot(x/1000,z,'r')
+
+% Converting time to a string to print string out
+trounded = round(t(i),0);
+strtime = num2str(trounded);
+
+
+txt = uicontrol('Style','text',...
+      'Position',[430 0 80 20],...
+      'Fontsize',12,...
+      'FontWeight','Bold',... 
+      'HorizontalAlignment','left',...
+      'String',['Time: ' strtime]);
+
+xlabel('distance (km)')
+ylabel('elevation (m)')
+title('Inception and Growth of Glacier over Time')
+
+
+pause(0.1)
+
+end
+
 % end
 
 
 end
-%hold off
+
+
+hold off
